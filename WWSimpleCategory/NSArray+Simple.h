683f89274db3f57f5a093c30d4708790b13816c9
WonWoo Choi <firewonwoo@gmail.com>
2014-09-16 23:18:04 +0900

    Create header file. Create NSArray category.

---
+++ WWSimpleCategory/NSArray+Simple.h
@@ -0,0 +1,20 @@
+//
+//  NSArray+Simple.h
+//  WWSimpleCategory
+//
+//  Created by ww on 14/9/16.
+//  Copyright (c) 2014년 Won Woo Choi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (JSON)
+
+/*!
+ @brief Array를 JSON으로 변환. Array에 포함된 객체는 NSString, NSNumber 등 문자열로 변환 가능한 객체이어야 함.
+ @brief ex) [a, b, c].
+ @return JSON 문자열.
+ */
+- (NSString *)JSONString;
+
+@end
---
+++ WWSimpleCategory/WWSimpleCategory.h
@@ -0,0 +1,15 @@
+//
+//  WWSimpleCategory.h
+//  WWSimpleCategoryProject
+//
+//  Created by ww on 14/9/16.
+//  Copyright (c) 2014년 Won Woo Choi. All rights reserved.
+//
+
+#ifndef WWSimpleCategoryProject_WWSimpleCategory_h
+#define WWSimpleCategoryProject_WWSimpleCategory_h
+
+#import "NSArray+Simple.h"
+
+
+#endif
